c0a30d43727f4c2765f127d9ab9f5c206560d1be
MatthewHYLai <matthewlai26714889@gmail.com>
2021-05-27 23:16:02 +0800

    Updates

---
+++ precache-manifest.8615312f52c1622c70fe9a94c23691c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7056badb657a35344677bc23513bb274",
+    "url": "/index.html"
+  },
+  {
+    "revision": "266c7fcd36030556f543",
+    "url": "/static/css/main.256aa2d3.chunk.css"
+  },
+  {
+    "revision": "91d689afca6d0e77df1a",
+    "url": "/static/js/2.92d0b128.chunk.js"
+  },
+  {
+    "revision": "729402817ca95f5c5c3f1c8f8d411609",
+    "url": "/static/js/2.92d0b128.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "266c7fcd36030556f543",
+    "url": "/static/js/main.975ee575.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
